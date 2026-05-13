fa90c4444d55c17101b220e011430d6c18d1787f
asjadon <jadonsinghankit31@gmail.com>
2026-05-13 12:07:21 +0000

    feat: update app to version 3.0

--- app/src/index.js
+++ app/src/index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from my DevOps project! Version 2.0',
-    version: process.env.APP_VERSION || '2.0.0',
+    message: 'Hello from my DevOps project! Version 3.0',
+    version: process.env.APP_VERSION || '3.0.0',
     environment: process.env.NODE_ENV || 'development'
   });
 });
